51ecc01362d627f53d302d1122f0667813c47d29
JdGXII <jralzaibar@yahoo.com>
2018-02-12 18:00:39 -0500

    Usuario ahora puede editar contenido del formulario en ultima pagina.

--- Cicero/wwwroot/js/app.js
+++ Cicero/wwwroot/js/app.js
@@ -1,6 +1,54 @@
 ﻿var app = angular.module('ciceroApp', []);
 
 app.controller('mainCtrl', function ($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
+
+    var demandante = { nombre: "", apellido: "", dni: "", telefono: "", email: "" }; 
+    var demandado = { nombre: "", ruc: "", telefono: "", email: "" };
+    var reclamo = { video: "", comentario: "Sin comentario" };
+    var solicitud = { seleccion: "", otro: "" };
+
+
+ 
+
+    $scope.demandante = demandante;
+    $scope.demandado = demandado;
+    $scope.reclamo = reclamo;
+    $scope.solicitud = solicitud;
+
+
+   
 });
+
+
+function previewVideo() {
+
+    var preview = document.querySelector('video');
+    var file = document.querySelector('#video').files[0];
+    var reader = new FileReader();
+
+    reader.onloadend = function () {
+        preview.src = reader.result;
+    }
+
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        preview.src = "";
+    }
+}
+
+function previewImage() {
+    var preview = document.querySelector('#foto-dni');
+    var file = document.querySelector('#input-dni').files[0];
+    var reader = new FileReader();
+
+    reader.onloadend = function () {
+        preview.src = reader.result;
+    }
+
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        preview.src = "";
+    }
+}
